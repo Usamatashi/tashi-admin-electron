05c0c833d4134c48461d4b60a15cece11fb45a32
Usamatashi <kumetewala@gmail.com>
2026-05-11 18:05:26 +0500

    Add electron/preload.cjs

---
+++ electron/preload.cjs
@@ -0,0 +1,11 @@
+"use strict";
+
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  saveConfig: (data) => ipcRenderer.invoke("save-config", data),
+  launchApp: () => ipcRenderer.invoke("launch-app"),
+  openFileDialog: () => ipcRenderer.invoke("open-file-dialog"),
+  getAppVersion: () => ipcRenderer.invoke("get-app-version"),
+  onServerCrashed: (cb) => ipcRenderer.on("server-crashed", (_e, code) => cb(code)),
+});
